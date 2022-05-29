291a75a4e3547a473b01b3bd390a8fbebb873391
Mxtreme22 <83869461+Mxtreme22@users.noreply.github.com>
2022-05-29 11:45:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+video = "";
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    document.getElementById("text").value;
+
+}
+
+function modelLoaded() {
+    console.log("Model loaded!");
+    status = true;
+}
